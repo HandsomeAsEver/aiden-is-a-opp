1c5222a75fe3a05c781439c4bdb0ae577be30e49
Kaylee is bussin <97471842+HandsomeAsEver@users.noreply.github.com>
2024-01-18 11:27:24 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+var tl = gsap.timeline({
+    defaults: {
+        duration: 1,
+    }
+});
+
+tl.to(".box1", {
+   scale: 0,
+   opacity: 0,
+   delay: 1,
+}).to(".box2", {
+    x: -200,
+
+}).to (".box3", {
+    x: -200,
+    borderRadious: 0,
+    rotate: 360,
+}).set(".box1", {
+    x:200,
+}).to(".box1", {
+    scale: 1,
+    opacity: 1,
+})
